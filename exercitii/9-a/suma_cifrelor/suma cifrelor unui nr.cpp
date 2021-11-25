f1a2addc4a37cd2fb4abcb5274036358a24bd08d
winnyace <winnyace82@gmail.com>
2021-11-25 18:09:11 +0200

    mici modificari la numele unor fisiere

--- exercitii/9-a/suma_cifrelor/suma cifrelor unui nr.cpp
+++ exercitii/9-a/suma_cifrelor/suma cifrelor unui nr.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int no, sc; // no = Numarul Original, sc = Suma cifrelor
+    int num, suma = 0;
     cout << "nr = "; cin >> no;
     sc = 0;
-    while(no != 0)
-    {
+    while(no != 0) {
         sc = sc + no % 10; // foarte simplu. suma este suma trecuta( la inceput 0) + ultima cifra al lui no.
         no = no / 10; // pentru a nu aduna acelasi nr pana la moartea universului, vom uita de ultima cifra al lui no asa.
     }
