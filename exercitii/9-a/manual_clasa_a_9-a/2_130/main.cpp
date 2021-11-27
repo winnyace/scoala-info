dfdb06bb67cf23d028c34b8374193ccbb6097a8d
winnyace <winnyace82@gmail.com>
2021-11-27 21:06:01 +0200

    2_130 modificat

--- exercitii/9-a/manual_clasa_a_9-a/2_130/main.cpp
+++ exercitii/9-a/manual_clasa_a_9-a/2_130/main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    // 2/130 sa se calculeze lungimea (2*r) si aria (pi*r*r)al unui cerc cu r dat de la tastatura.
-    int r;
-    float a, l, PI = 3.14;
-    cout << "r = "; cin >> r;
-    a = PI*(rcr*r);
-    l = 2*r;
-    cout << endl << a;
-    cout << endl << l;
+    // 2/130 să se calculeze lungimea (2*raza) și aria (pi*raza*raza) unui cerc cu raza dat de la tastatură.
+    int raza;
+    float aria, lungimea, PI = 3.14;
+    cout << "voi calcula lungimea si aria unui cerc. doar spune-mi raza.\n"; cin >> raza;
+    aria = PI*(raza*raza);
+    lungimea = 2*raza;
+    cout << aria;
+    cout << endl << lungimea;
     return 0;
 }
