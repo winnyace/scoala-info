680ec3cc967a4de93d0ad44496253f1d02786ac9
winnyace <winnyace82@gmail.com>
2021-11-27 11:13:23 +0200

    comentariu de ce num != 0 in while

--- exercitii/9-a/suma_cifrelor/suma cifrelor unui nr.cpp
+++ exercitii/9-a/suma_cifrelor/suma cifrelor unui nr.cpp
@@ -7,7 +7,8 @@ int main()
     int num, suma = 0;
     cout << "o sa calculez suma cifrelor unui numar. spune-mi numarul:"; cin >> num;
     while (num != 0) {
-        suma += num%10; 
+        // am învățat că div/10 scapă de cifrele unui număr. când scăpam de ultima cifră, numărul rămas e 0.
+        suma += num%10;
         num /= 10;
     }
     cout << "suma cifrelor este " << suma;
