768d72165a8c7a23f25160e31ed20024df2f9494
winnyace <winnyace82@gmail.com>
2021-11-27 11:55:20 +0200

    redenumire fișiere, modificării aduse unor programe

--- exercitii/9-a/cate_nr/cate_nr.cpp
+++ exercitii/9-a/cate_nr/cate_nr.cpp
@@ -3,27 +3,27 @@ using namespace std;
 
 int main()
 {
-	/* se citesc nr intregi de la tastatura pana la citirea lui 13.
-	 * afisati cate nr positive, negative si nule s-au citit. */
-	int n, nplus = 0, nminus = 0, nnul = 0, sw = 0; 
-	/* nplus pt nr positive
-	 * nminus pt nr negative
-	 * nnul pt 0 */
-	cout << "dati val lui n(13 pt stop): "; cin >> n;
-	while (n != 13) {
-		sw = 1; // folosim acest sw pt a nu oferi la final 0 in caz ca nu s-au citit nr. il putem aici deoarece putem si ajuta utilizatorul
-		if (n > 0)
-			nplus++;
-		else if (n < 0)
-			nminus++;
+	/* se citesc nr întregi de la tastatură până la citirea lui 13.
+	 * afisați câte nr positive, negative și nule s-au citit. */
+	int num, numPlus = 0, numMinus = 0, numNul = 0, sw = 0;
+	/* numPlus pt nr positive
+	 * numMinus pt nr negative
+	 * numNul pt 0 */
+	cout << "dati val lui num(13 pt stop): "; cin >> num;
+	while (num != 13) {
+		sw = 1; // folosim acestă variabilă pentru output-ul programului.
+        if (num > 0)
+			numPlus++;
+		else if (num < 0)
+			numMinus++;
 		else
-			nnul++;
-		cout << "dati val lui n(13 pt stop): "; cin >> n;
+			numNul++;
+		cout << "dati val lui num(13 pt stop): "; cin >> num;
 	}
 	if (sw == 1) { // iar aici este exact ce vorbeam mai sus.
-		cout << "nr positive citite " << nplus;
-		cout << endl << "nr negative citite " << nminus;
-		cout << endl << "nr nule citite " << nnul;
+		cout << "nr positive citite " << numPlus;
+		cout << endl << "nr negative citite " << numMinus;
+		cout << endl << "nr nule citite " << numNul;
 	}
 	else
 		cout << "nu s-au citit numere";
