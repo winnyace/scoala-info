92c6103232746f33a0b38d339f9e9a8af34005eb
winnyace <winnyace82@gmail.com>
2021-11-28 18:37:18 +0200

    4 și 3 modificat + gitignore

--- exercitii/9-a/manual_clasa_a_9-a/3_169/main.cpp
+++ exercitii/9-a/manual_clasa_a_9-a/3_169/main.cpp
@@ -5,6 +5,7 @@ int main()
 {
 	// 3/169 să se afiseze cel mic element dintr-un vector și posiția sa.
 	int numel, pos, min = 0;
+    cout << "o sa gasesc cel mai mic element din vector.\n";
 	cout << "cate elemente vrei in vector?\n"; cin >> numel;
 	int arr[numel];
 	cout << "elementul 0 = "; cin >> arr[0]; min = arr[0]; // luăm primul element pentru a compara cu restul de elemente.
--- exercitii/9-a/manual_clasa_a_9-a/4_169/main.cpp
+++ exercitii/9-a/manual_clasa_a_9-a/4_169/main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
-
 using namespace std;
 
-int main() {
-	// 4 /169 sa se afiseze nr pare si apoi nr impare dintr-un vector.
-	int n, i;
-	cout << "nr de elemente a listei:"; cin >> n;
-	int arr[n];
-	for(i = 0;i < n; i++) {
-		cout << "un element:"; cin >> arr[i];
-	}
-	for(i = 0; i < n; i++) {
-		if(arr[i] % 2 == 0) {
-			cout << arr[i] << " ";
-		}
-	}
-	for(i = 0; i < n; i++) {
-		if(arr[i] % 2 != 0) {
-			cout << arr[i] << " ";
-		}
-	}
+int main()
+{
+	// 4/169 să se afiseze nr pare și apoi nr impare dintr-un vector.
+    int numel;
+    cout << "o să scriu elementele pare și impare dintr-un vector.\n";
+    cout << "câte elemente vrei in vector?\n"; cin >> numel;
+    int arr[numel];
+    for (int i = 0; i < numel; i++) {
+        cout << "elementul " << i << " = "; cin >> arr[i];
+    }
+    cout << "numerele pare din vector:\n";
+    for (int i = 0; i < numel; i++)
+        if (arr[i]%2 == 0) // împărțirea cu rest a unui număr cu 2 este 0 dacă numărul este par.
+            cout << arr[i] << " ";
+    cout << "\nnumerele impare din vector:\n";
+    for (int i = 0; i < numel; i++)
+        if (arr[i]%2 != 0) // dacă dă restul diferent de 0, e clar nu e par.
+            cout << arr[i] << " ";
 	return 0;
 }
