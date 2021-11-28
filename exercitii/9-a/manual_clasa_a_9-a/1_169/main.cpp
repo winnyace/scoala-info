ae206c30c2b3d10f6cba3761a2f699342924e83a
winnyace <winnyace82@gmail.com>
2021-11-28 18:22:17 +0200

    nume variabile; 3_169 modificat ca max_vector

--- exercitii/9-a/manual_clasa_a_9-a/1_169/main.cpp
+++ exercitii/9-a/manual_clasa_a_9-a/1_169/main.cpp
@@ -6,14 +6,14 @@ int main()
     // 1/167 să se afiseze MA dintre numerele positive dintr-un vector.
     int numel, i, ma = 0;
     cout << "voi face MA a numerelor positive dintr-un vector.\n";
-    cout << "câte elemente vrei în vector?\n"; cin >> numel;
-    int arr[numel]; // poti defeni un array/vector si asa decat sa-l definesti arr[val]. tine minte ca iti trebuie prima data n.
-    for(i = 0; i < numel; i++) {
+    cout << "câte elemente vrei in vector?\n"; cin >> numel;
+    int arr[numel]; // poți defeni un array/vector și asa decât să-l definesti arr[val]. ține minte că îți trebuie prima data n citit.
+    for (i = 0; i < numel; i++) {
 		cout << "elementul " << i << " = "; cin >> arr[i];
 		if(arr[i] > 0)
 			ma += arr[i];
     }
-    if(ma > 0)
+    if (ma > 0)
         cout << float(ma / numel); // este foarte probabil ca numerele să dea cu virgulă, așa că vom avem resultul de element float. facem un cast pentru acestă operație.
 	else
 		cout << "nu am gasit numere positive in vector.";
--- exercitii/9-a/vectori_si_matrici/element_max_din_vector/max_vector.cpp
+++ exercitii/9-a/vectori_si_matrici/element_max_din_vector/max_vector.cpp
@@ -5,19 +5,18 @@ int main()
 {
 	// 1. găsesțe cel mai mare element dintr-un vector + poziția sa.
     // sursă: https://www.w3resource.com/cpp-exercises/array/index.php
-	int n, i, max;
-	cout << "câte elemente vrei în vector?\n"; cin >> n;
-	int arr[n];
-	cout << "elementul 0 = "; cin >> arr[0]; max = arr[0];
-	for(i = 1; i < n; i++) {
+	int numel, pos, max;
+	cout << "câte elemente vrei în vector?\n"; cin >> numel;
+	int arr[numel];
+	cout << "elementul 0 = "; cin >> arr[0]; max = arr[0]; // luăm primul element pentru a compara cu restul de elemente.
+	for (int i = 1; i < numel; i++) {
 		cout << "elementul " << i << " = "; cin >> arr[i];
-		if(max < arr[i])
+		if (max < arr[i]) {
         	max = arr[i];
+            pos = i;
+        }
 	}
-	cout << endl << "cel mai mare element:" << max << endl;
-	cout << "pozitia pe care s-a gasit max:";
-	for(i = 0; i < n; i++)
-		if(max == arr[i])
-			cout << i << " ";
+    cout << "elementul maxim din vector este " << max;
+    cout << "\nsi prima positie pe care am gasit el max este " << pos;
 	return 0;
 }
