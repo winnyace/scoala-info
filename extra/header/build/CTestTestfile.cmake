90fb402566c2fd9c0835405c5170cc9ef6a1ec07
winnyace <winnyace82@gmail.com>
2022-04-02 11:04:32 +0300

    extra schimbări.

---
+++ extra/header/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ion/Documents/Projects/C++/teorie/extra/header
+# Build directory: /home/ion/Documents/Projects/C++/teorie/extra/header/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
