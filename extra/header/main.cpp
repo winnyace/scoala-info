b4c2417efe8d4675b693b1d0e1c1d5e4674ceb9c
winnyace <winnyace82@gmail.com>
2022-04-02 11:10:17 +0300

    comentat asupra unui ex. extra.

--- extra/header/main.cpp
+++ extra/header/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-#include "main.h"
+#include "main.h" // așa definim un fișier de tip header.
+
+/* pentru a compila cu un fișier de acest tip, este necesar să specificăm fișierul.
+ * am decis să folosc CMake pentru a crea fișierul compilat. */
 
 int main() {
-    salut();
+    salut(); // salut e definit în main.h
     return 0;
 }
