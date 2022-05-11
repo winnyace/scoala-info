d6ef10c86aac1cc5f87f45231b2a24e547869ff7
winnyace <winnyace82@gmail.com>
2022-05-11 09:48:55 +0300

    stergere liste dublu înlănțuite

---
+++ teorie/liste_înlănțuite/LDI/stergere/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct nod {int info; nod *urm, *ant;};
+
+void creare(nod *&prim, nod *&ultim)
+{
+    ifstream f("lista"); // am citit lista dintr-un fisier text. de ce? pentru că vreau doar să arat funcțiile de stergere.
+    int n;
+    nod *nou = new nod;
+    while (f >> n) {
+        nou->info = n;
+        if (prim == NULL) {
+            prim = nou;
+            prim->ant = NULL;
+            prim->urm = NULL;
+            ultim = prim;
+        }
+        else {
+            nou->ant = ultim;
+            nou->urm = NULL;
+            ultim->urm = nou;
+            ultim = nou;
+        }
+        nou = new nod; // nu uita!
+    }
+}
+
+void afisare(nod *prim)
+{
+    nod *p = prim;
+    cout << "\nlista curenta este: ";
+    while (p != NULL) {
+        cout << p->info << " ";
+        p = p->urm;
+    }
+}
+
+void sterg_prim(nod *&prim)
+{
+    cout << "\nsterg primul nod din lista curenta, acest nod fiind " << prim->info << endl;
+    nod *p = prim;
+    prim = prim->urm;
+    prim->urm->ant = NULL;
+    delete p;
+}
+
+int main()
+{
+    nod *prim, *ultim;
+    prim = new nod; ultim = new nod;
+    prim = ultim = NULL;
+    creare(prim, ultim); afisare(prim);
+    cout << endl;
+    sterg_prim(prim); afisare(prim);
+    return 0;
+}
