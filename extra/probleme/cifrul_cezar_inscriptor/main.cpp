ccceabb4ecf8e595879423b0f80fa00137fbfb85
winnyace <winnyace82@gmail.com>
2022-08-16 14:34:56 +0300

    rescris cifrul lui cezar 2: electric boogaloo

--- extra/probleme/cifrul_cezar_inscriptor/main.cpp
+++ extra/probleme/cifrul_cezar_inscriptor/main.cpp
@@ -1,31 +1,73 @@
 #include <iostream>
 #include <string>
 
-// https://en.wikipedia.org/wiki/Caesar_cipher
-
 int main()
 {
     std::string input;
     int n;
     char aux;
-    std::getline(std::cin, input);
-    std::cin >> n;
+    std::cout << "fraza:"; std::getline(std::cin, input);
+    std::cout << "permutare cu:"; std::cin >> n;
 
     std::string res;
     for (int i = 0; i < input.size(); i++)
     {
-        if (input.at(i) >= 'a' && input.at(i) <= 'z')
+        if (input.at(i) >= 'a' && input.at(i) <= 'z') 
+        {
             res += input.at(i)+n;
+            if (res.at(i) > 'z')
+            {
+                aux = res.at(i);
+                res.pop_back();
+                res += aux-26; 
+            }
+        }
+
+        if (input.at(i) >= 'A' && input.at(i) <= 'Z')
+        {
+            res += input.at(i)+n;
+            if (res.at(i) > 'Z')
+            {
+                aux = res.at(i);
+                res.pop_back();
+                res += aux-26; 
+            }
+        }
+
         if (input.at(i) == ' ') 
             res += ' ';
-        
-        if (res.at(i) > 'z')
+    }
+    
+    std::string res2;
+    for (int i = 0; i < res.size(); i++)
+    {
+        if (res.at(i) >= 'a' && res.at(i) <= 'z') 
         {
-            aux = res.at(i);
-            res.pop_back();
-            res += aux-26; 
+            res2 += res.at(i)-n;
+            if (res2.at(i) < 'a')
+            {
+                aux = res2.at(i);
+                res2.pop_back();
+                res2 += aux+26; 
+            }
         }
+
+        if (res.at(i) >= 'A' && res.at(i) <= 'Z')
+        {
+            res2 += res.at(i)-n;
+            if (res2.at(i) < 'A')
+            {
+                aux = res2.at(i);
+                res2.pop_back();
+                res2 += aux+26; 
+            }
+        }
+
+        if (res.at(i) == ' ') 
+            res2 += ' ';
     }
 
-    std::cout << res;
+    std::cout << "\nencrypted: " << res;
+    std::cout << "\ndecrypted: " << res2;
+    return 0;
 }
